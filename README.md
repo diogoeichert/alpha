8e21520c09f04e7c8abdfb3dbce01ff4769af971
Diogo Eichert <diogoeichert@icloud.com>
2021-02-06 12:14:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# alpha
+Single-player adventure
---
+++ maps/tileset.tsx
@@ -0,0 +1,94 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tileset" tilewidth="32" tileheight="32" tilecount="29" columns="0">
+ <editorsettings>
+  <export target="." format=""/>
+ </editorsettings>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="tiles/00.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="tiles/01.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="tiles/10.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="tiles/11.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="tiles/12.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="tiles/13.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="tiles/14.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="tiles/15.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="tiles/16.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="tiles/17.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="tiles/18.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="tiles/19.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="tiles/20.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="tiles/21.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="tiles/22.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="tiles/23.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="tiles/24.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="tiles/25.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="tiles/26.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="tiles/27.png"/>
+ </tile>
+ <tile id="20">
+  <image width="32" height="32" source="tiles/28.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="tiles/29.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="tiles/30.png"/>
+ </tile>
+ <tile id="23">
+  <image width="32" height="32" source="tiles/31.png"/>
+ </tile>
+ <tile id="24">
+  <image width="32" height="32" source="tiles/32.png"/>
+ </tile>
+ <tile id="25">
+  <image width="32" height="32" source="tiles/33.png"/>
+ </tile>
+ <tile id="26">
+  <image width="32" height="32" source="tiles/34.png"/>
+ </tile>
+ <tile id="27">
+  <image width="32" height="32" source="tiles/35.png"/>
+ </tile>
+ <tile id="28">
+  <image width="32" height="32" source="tiles/36.png"/>
+ </tile>
+</tileset>
